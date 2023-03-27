d6569218dc093764c68f133930a00fe005a4d389
Praks <aim.prakhar@gmail.com>
2023-03-27 22:27:46 +0530

    all

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,30 +1,31 @@
 import express from "express";
-import { createRoom, deleteRoom, getRoom, getRooms, updateRoom } from "../controllers/Room.js";
-const router =express.Router();
+import {
+  createRoom,
+  deleteRoom,
+  getRoom,
+  getRooms,
+  updateRoom,
+} from "../controllers/Room.js";
+const router = express.Router();
 
 import { createError } from "../utils/error.js";
 import { verifyAdmin, verifyUser } from "../utils/verifyToken.js";
 
-
-
-router.get("/:id",getRoom);
+router.get("/:id", getRoom);
 //POST
 
-router.post("/:hotelid",verifyAdmin,createRoom);
-
-
-//UPDATE
-
-router.put("/:id",verifyAdmin,updateRoom);
+router.post("/:hotelid", verifyAdmin, createRoom);
 
 //UPDATE
 
-router.delete("/:id/:hotelId",verifyAdmin,deleteRoom);
+router.put("/:id", verifyAdmin, updateRoom);
+
+//UPDATE
+
+router.delete("/:id/:hotelId", verifyAdmin, deleteRoom);
 
 //GET ALL
 
-router.get("/",getRooms)
-
-
+router.get("/", getRooms);
 
 export default router;
