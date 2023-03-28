dddc79d0d7c91ec4274787039d5acecbbae6723c
Praks <aim.prakhar@gmail.com>
2023-03-28 09:10:12 +0530

    ff

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { deleteUser, getUser, getUsers, updateUser } from "../controllers/Users.js";
+import { deleteUser, getUser, getUsers, updateUser } from "../controllers/users.js";
 import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 const router =express.Router();
 
